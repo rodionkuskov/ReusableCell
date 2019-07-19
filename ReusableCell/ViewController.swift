856d3c251f63f7c325ee416a6c68c38486f57175
Rodion <rodion@dsi.io>
2019-07-19 18:00:50 +0300

    Initial Commit

---
+++ ReusableCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReusableCell
+//
+//  Created by Rodion on 7/19/19.
+//  Copyright © 2019 Rodion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
